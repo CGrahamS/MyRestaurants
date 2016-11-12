05809786c2a706b7da1710f77b6f8bac40230d73
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-11-12 12:42:51 -0800

    finish "Intents" lesson

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -1,5 +1,6 @@
 package com.epicodus.myrestaurants;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,12 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mFindRestuarantsButton = (Button) findViewById(R.id.findRestaurantsButton);
-            mFindRestuarantsButton.setOnClickListener(new View.OnClickListener() {
+        mFindRestuarantsButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                    startActivity(intent);
                 }
             });
 
